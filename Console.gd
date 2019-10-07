53a12e0491a455324f07cdf2af1e2770840e8582
Tristan Shelton <tristan.m.shelton@gmail.com>
2019-10-07 13:19:57 -0400

    Improve console output

--- Console.gd
+++ Console.gd
@@ -1,9 +1,18 @@
 extends TextEdit
 
+var outputDelta = 0
+var outputDelay: float = 0.1 / 3
+var outputBuffer = ''
+var allowInput = false
+
 func _ready() -> void:
 	grab_focus()
 	
 func _input(event):
+	if not allowInput:
+		get_tree().set_input_as_handled()
+		return
+		
 	if event is InputEventKey:
 		if event.is_pressed():
 			if event.scancode == KEY_UP or event.scancode == KEY_DOWN:
@@ -15,18 +24,29 @@ func _input(event):
 				var input = get_line(cursor_get_line()).right(2)
 				text += '\n'
 				handleInput(input)
-				text += '\n'
 				createPrompt()
 				get_tree().set_input_as_handled()
 				
 func output(string):
-	text += string
-	yield(get_tree().create_timer(0.01), 'timeout')
-	cursor_set_line(get_line_count(), true, false)
+	outputBuffer += string
+	
+func _process(delta):
+	if outputBuffer.length() == 0:
+		outputDelta = 0
+		allowInput = true
+		return
+		
+	outputDelta += delta
+	
+	while outputDelta > outputDelay and outputBuffer.length() > 0:
+		text += outputBuffer.substr(0, 1)
+		outputBuffer = outputBuffer.substr(1, outputBuffer.length())
+		cursor_set_line(get_line_count(), true, false)
+		cursor_set_column(get_line(get_line_count() - 1).length())
+		outputDelta -= outputDelay
 	
 func createPrompt():
-	output('> ')
-	cursor_set_column(2)
+	output('\n> ')
 	
 func handleInput(string: String):
 	var methodName = 'command_' + string.to_lower().strip_edges()
