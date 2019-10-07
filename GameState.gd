d7db4c8178cc6a8a300372745781999fe8f8bbdb
Tristan Shelton <tristan.m.shelton@gmail.com>
2019-10-07 17:59:47 -0400

    Ship minigame

--- GameState.gd
+++ GameState.gd
@@ -6,3 +6,12 @@ var miniGameResult = false
 
 func goToConsole():
 	get_tree().change_scene_to(consoleScene)
+	
+func playSfx(audioStream, volume = 0):
+	var sfx = AudioStreamPlayer.new()
+	sfx.stream = audioStream
+	sfx.volume_db = volume
+	get_tree().current_scene.add_child(sfx)
+	sfx.play()
+	yield(sfx, 'finished')
+	sfx.queue_free()
